6e4fec5856d8301e8a9804d59c3b7e79f7275f60
sfpgmr <sfpgmr@example.com>
2015-10-13 06:25:16 +0900

    remix update

--- src/js/remix.js
+++ src/js/remix.js
@@ -1,5 +1,21 @@
 import Slider from '../js/slider.js';
 import ipc from 'ipc';
+import remote from 'remote';
+var fs = remote.require('fs');
+import denodeify from '../js/denodeify.js';
+
+var readFile = denodeify(fs.readFile);
+var audioCtx = new AudioContext();
+var resources = [];
+
+function toArrayBuffer(buffer) {
+    var ab = new ArrayBuffer(buffer.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return ab;
+}
 
 window.onload = ()=>{
 	var mes = d3.select('body')
@@ -18,15 +34,55 @@ window.onload = ()=>{
 	ipc.on('resources',(r)=>{
 		clearInterval(id);
 		mes.remove();
+		resources = r;
+
 		var svg = d3.select('body').append('svg')
 		.attr({width:1024,height:768});
-	
-		for(var i = 0;i < 6;++i){
-			new Slider(svg,{x:50 + i * 70,y:768 - 240});
+		
+		var promise = Promise.resolve();
+		var sources = [];
+		
+		for(var i = 0;i < 7;++i){
+			promise = promise.then(readFile.bind(null,resources[i].path))
+			.then((data)=>{
+				return new Promise((resolve,reject)=>{
+					var ab = toArrayBuffer(data); 
+					audioCtx.decodeAudioData(ab,(buffer)=>{
+						resolve(buffer);
+					},(err)=>{reject(new Error('decodeAudioData():error:'));});
+				});
+			})
+			.then(((idx)=> { 
+				return (buffer)=>{
+					var source = audioCtx.createBufferSource();
+					source.buffer = buffer;
+					source.loop = false;
+					sources.push(source);
+					var gain = audioCtx.createGain();
+					source.connect(gain);
+					gain.connect(audioCtx.destination);
+					var slider = new Slider(svg,{x:50 + idx * 70,y:768 - 240});
+					gain.gain.value = slider.value = 0.5;
+					slider.on('change',(v)=>{
+						gain.gain.value = v;
+					});
+				};
+			})(i))
+			.catch((e)=>{
+				console.log(e);
+			});				
 		}
-
-		console.log(r);
+		
+		promise
+		.then(()=>{
+			console.log('play');
+			var current = audioCtx.currentTime + 0.01;
+			sources.forEach((s)=>{
+				s.start(current);
+			});
+		});
 	});
+		
 	
 	// var slider = new Slider(svg,{x:50,y:768 - 220});
 	// var slider2 = new Slider(svg,{x:120,y:768 - 220});
